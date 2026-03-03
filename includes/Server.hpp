487516e395e63f81c46cf74bd64e651df78f8504
Bryan <btjosueturcios@gmail.com>
2026-03-03 09:10:12 +0100

    home

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 14:57:12 by brturcio          #+#    #+#             */
-/*   Updated: 2026/02/28 22:16:57 by brturcio         ###   ########.fr       */
+/*   Updated: 2026/03/02 18:23:12 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,17 @@ private:
 	int						_port;
 	int						_serSocketFd;
 	std::string				_pass;
-	std::vector<Client>		_clients;
+	//std::vector<Client>		_clients;
 	std::vector<pollfd>		_pollFds;
 
 public:
 	Server(int port, const std::string& pass);
 	~Server(void);
 
-	void    createSocket();   // call socket()
-	void    bindSocket();     // call a bind()
-	void    listenSocket();   // call a listen()
-	int     acceptClient();   // call a accept(), return fd del cliente
+	void    createSocket();
+	void    bindSocket();
+	void    listenSocket();
+	int     acceptClient();
 	void    receiveData(int client_fd);
 	void    sendData(int client_fd, const std::string& msg);
 };
--- sources/mandatory/Server.cpp
+++ sources/mandatory/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: brturcio <brturcio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 14:57:08 by brturcio          #+#    #+#             */
-/*   Updated: 2026/02/28 18:00:23 by brturcio         ###   ########.fr       */
+/*   Updated: 2026/03/02 16:14:02 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ Server::Server(int port, const std::string& pass) :
 	_port(port),
 	_serSocketFd(-1),
 	_pass(pass),
+	//_clients(),
 	_pollFds()
 {}
 
