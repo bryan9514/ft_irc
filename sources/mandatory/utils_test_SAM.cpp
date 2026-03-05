ba6d339466d03998ece0e7e4b2b9c6b4ca4144ad
SamFighter_ <samullabbe@gmail.com>
2026-03-05 11:32:46 +0100

    Worked on some utils, started to look for many error msg, did the send msg - Sam

--- sources/mandatory/utils_test_SAM.cpp
+++ sources/mandatory/utils_test_SAM.cpp
@@ -23,7 +23,7 @@ std::vector<std::string>	Tokenizer(std::string& line)
 	return (token);
 }
 
-int		handleCmd(/*Client &Client,*/ std::string& buffer)
+int		handleCmd(Client &Client,std::string& buffer)
 {
 	std::vector<std::string> splitted = this->Tokenizer(buffer);
 	unsigned int size = splited.size();
@@ -36,8 +36,38 @@ int		handleCmd(/*Client &Client,*/ std::string& buffer)
 			std::cout << "\"" << splited[i] << "\"" << " ";
 	}
 	std::cout << std::endl;
-//	std::string commands[??] = { "NICK" , "TOPIC" , "JOIN", "KICK", "INVITE", "PRIVMSG" , "QUIT" ...}
-//	for (int i = 0; i <= ??; i++)
-//		if (splitted[i] == commands[i])
-//			break;
+	std::string commands[??] = { "PASS" , "QUIT" , "NICK", "USER", "KICK", "INVITE" , "TOPIC" ...}
+	for (int i = 0; i <= ??; i++)
+	{
+		if (splitted[0] == commands[i])
+			break;
+	}
+	if (i > 1 && !client->_is_Logged)
+	{
+		std::cout << "Register by using 'PASS <password>'\r\n" << std::endl;
+		return (0);
+	}
+	if (i > 3 && !client->_is_registered)
+	{
+		std::cout << "Start logging in by using 'USER <username>\r\n" << std::endl;
+		return (0);
+	}
+	switch (i) {
+		case 0:
+		  this->pass(client, splitted);
+		  return (0);
+		case 1:
+			//[...]
+	}
+}
+
+void	/*Server::*/displayMsg(std::string str, int clientFD)
+{
+	send(clientFD, str.c_str(), str.length(), 0);
+}
+
+void /*Server::*/sendToAll(const std::string& msg)
+{
+    for (std::map<int, Client>::iterator it = _clients.begin(); it != _clients.end(); ++it)
+        send(it->first, msg.c_str(), msg.length(), 0);
 }
