70898993014038d2c035cd5da87615ba93c850dd
bryan9514 <btjosueturcios@gmail.com>
2026-04-10 08:14:27 +0200

    parsing main

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ntome <ntome@42angouleme.fr>               +#+  +:+       +#+        */
+/*   By: brturcio <brturcio@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/27 11:00:10 by brturcio          #+#    #+#             */
-/*   Updated: 2026/03/16 10:57:08 by ntome            ###   ########.fr       */
+/*   Updated: 2026/04/10 08:00:13 by brturcio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,46 @@
 #include <stdlib.h>
 #include <exception>
 #include <iostream>
+#include <cerrno>
+#include <string.h>
 
 volatile sig_atomic_t gSignalStatus = 0;
 void	shutdownServer(void);
+
+static bool isNumber(const char *str)
+{
+	if (!str || !*str)
+		return (false);
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (false);
+	}
+	return (true);
+}
+
+static int parsePort(const char *str)
+{
+	char *end;
+	long port;
+
+	if (!isNumber(str))
+		throw std::runtime_error("port must be numeric");
+	errno = 0;
+	port = std::strtol(str, &end, 10);
+	if (errno != 0 || *end != '\0')
+		throw std::runtime_error("invalid port");
+	if (port < 1024 || port > 65535)
+		throw std::runtime_error("port must be between 1024 and 65535");
+	return (static_cast<int>(port));
+}
+
+static std::string parsePassword(const char *str)
+{
+	if (!str || strlen(str) == 0)
+		throw std::runtime_error("password cannot be empty");
+	return (std::string(str));
+}
 
 int	main(int ac, char **av)
 {
@@ -26,11 +63,13 @@ int	main(int ac, char **av)
 		return (1);
 	}
 	try {
-		Server	s(std::atoi(av[1]), av[2]);
+		int port = parsePort(av[1]);
+		std::string password = parsePassword(av[2]);
+		Server	s(port, password);
 
 		signal(SIGPIPE, SIG_IGN);
-        signal(SIGINT, signalHandler);
-        signal(SIGQUIT, signalHandler);
+		signal(SIGINT, signalHandler);
+		signal(SIGQUIT, signalHandler);
 		s.initServer();
 		s.runServer();
 		if (gSignalStatus)
